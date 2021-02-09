e822369f24a50434b09cc9aba03fe89fa36b636c
Mengdi Liu <liumengdi923@gmail.com>
2021-02-08 23:20:29 -0500

    added queries.sql and exported retirement_info.csv from the query

---
+++ queries.sql
@@ -0,0 +1,42 @@
+select * from employees
+limit 10;
+
+select first_name, last_name
+from employees
+where birth_date between '1952-01-01' and '1955-12-31';
+
+select first_name, last_name
+from employees
+where birth_date between '1952-01-01' and '1952-12-31'
+
+select first_name, last_name
+from employees
+where birth_date between '1953-01-01' and '1953-12-31'
+
+select first_name, last_name
+from employees
+where birth_date between '1954-01-01' and '1954-12-31'
+
+select first_name, last_name
+from employees
+where birth_date between '1955-01-01' and '1955-12-31'
+
+-- Retirement eligibility
+select first_name, last_name
+from employees
+where (birth_date between '1952-01-01' and '1955-12-31')
+and (hire_date between '1985-01-01' and '1988-12-31');
+
+-- Number of employees retiring
+select count(first_name)
+from employees
+where (birth_date between '1952-01-01' and '1955-12-31')
+and (hire_date between '1985-01-01' and '1988-12-31');
+
+select first_name, last_name
+into retirement_info
+from employees
+where (birth_date between '1952-01-01' and '1955-12-31')
+and (hire_date between '1985-01-01' and '1988-12-31');
+
+select * from retirement_info;
